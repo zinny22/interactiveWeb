562038605c4c8104b3f7dd6f0f3d65c45c8c2808
zinny22 <hyejinsang@naver.com>
2024-02-12 01:19:28 +0900

    feat : canvas 불꽃놀이

---
+++ 불꽃놀이/js/Particle.js
@@ -0,0 +1,35 @@
+import CanvasOption from "./CanvasOption.js";
+
+export default class Particle extends CanvasOption {
+  constructor(x, y, vx, vy, opacity, colorDeg) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.opacity = opacity;
+    this.colorDeg = colorDeg;
+    this.gravity = 0.12;
+    this.friction = 0.93;
+  }
+
+  update() {
+    this.vy += this.gravity;
+
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.opacity -= 0.02;
+  }
+
+  draw() {
+    this.ctx.fillStyle = `hsla(${this.colorDeg},100%, 65%, ${this.opacity})`;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+}
